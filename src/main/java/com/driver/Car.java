7a5016454b347f955d0f04c1c26ea355b2ad3142
Harsh Lalwani <harshlalwani660@gmail.com>
2022-12-26 21:12:05 +0530

    OK YES

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -20,7 +20,7 @@ public class Car extends Vehicle {
         this.isManual=isManual;
         this.type=type;
         this.seats=seats;
-        this.currentGear=0;
+        this.currentGear=1;
     }
 
     public int getWheels() {
